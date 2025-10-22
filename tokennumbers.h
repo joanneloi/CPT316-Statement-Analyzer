513651f378032e6a0df4a537100d73b827ea219c
Joanne Loi <joanneloijinxu@student.usm.my>
2025-10-22 12:33:08 +0800

    new file:   .vscode/c_cpp_properties.json 	new file:   .vscode/launch.json 	new file:   .vscode/settings.json 	new file:   .vscode/tasks.json 	new file:   Syntax_Analyzer 	new file:   Syntax_Analyzer.cpp 	new file:   lex.h 	new file:   syntax.cpp 	new file:   syntax.py 	new file:   tokennumbers.h

---
+++ tokennumbers.h
@@ -0,0 +1,18 @@
+#ifndef TOKENNUMBERS_H
+#define TOKENNUMBERS_H
+
+enum Token {
+    ID,         // identifier
+    INT,        // integer
+    ASSIGN,     // =
+    PLUS,       // +
+    MINUS,      // -
+    TIMES,      // *
+    DIVIDE,     // /
+    LPAREN,     // (
+    RPAREN,     // )
+    SEMI,       // ;
+    END         // end of input
+};
+
+#endif
